36c1b714b96254803845c05077c2a6c866ce39cd
Maheshkadam-Delxn <maheshkadam.delxn@gmail.com>
2025-04-01 20:42:15 +0530

    changes are done

--- myapp/src/app/layout.js
+++ myapp/src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/image.png" />
+        <link rel="icon" href="/image.png" type="image/png" />
         {/* Preconnect for performance optimization */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
